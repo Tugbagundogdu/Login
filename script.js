d0674af033faddcd2db818f238048bb87f13de35
Tugbagundogdu <78304413+Tugbagundogdu@users.noreply.github.com>
2022-05-27 04:44:39 +0300

    Login

---
+++ script.js
@@ -0,0 +1,56 @@
+var kullanicilar = [{ kullaniciAdi : "admin" , parola: "admin" },
+{ kullaniciAdi : "tugba" , parola: "1234" },
+{ kullaniciAdi : "javascript" , parola: "abcdef" }
+
+
+];
+
+var girilenKullaniciAdi;
+var girilenParola ;
+
+var x ;
+
+function kontrolEt (){
+
+    girilenKullaniciAdi = document.getElementById("kullaniciAdi").value ;
+    girilenParola = document.getElementById("parola").value;
+
+    for (x of kullanicilar){
+
+        if((x.kullaniciAdi == girilenKullaniciAdi) && (x.parola == girilenParola)){
+
+            return true;
+        }
+
+        else{
+
+            return false;
+
+        }
+
+    }
+
+};
+
+function girisOnayi(){
+
+kontrolEt()
+
+if(kontrolEt()){
+
+alert("HOŞŞGELDİNİZ EFENİİİMM");
+
+}
+
+else{
+
+    alert("YANLIŞ KULLANICI ADI YADA PAROLA");
+    document.getElementById("kullaniciAdi").value = "";
+    document.getElementById("parola").value = "";
+
+}
+
+
+};
+
+
